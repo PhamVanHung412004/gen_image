314bacfcecab1d0036d2963c7a132dd261f79847
Pham Van Hung <137643495+PhamVanHung412004@users.noreply.github.com>
2025-08-01 19:53:54 +0700

    Delete get_dataset directory

--- get_dataset/download_dataset.py
+++
@@ -1,3 +0,0 @@
-from datasets import load_dataset
-# or load the separate splits if the dataset has train/validation/test splits
-valid_dataset = load_dataset("asgaardlab/GameplayCaptions", split="validation")
